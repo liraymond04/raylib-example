8c23b458bef0955cbb047261dc8730ee7d7337be
Raymond Li <li.raymond04@gmail.com>
2023-08-21 17:42:31 -0700

    Add emscripten building to build script

--- build.sh
+++ build.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
-cmake -B build
-cmake --build build
+
+target_flag=''
+
+while getopts ':t:' flag; do
+  case "${flag}" in
+    t) target_flag="$OPTARG" ;;
+    *) exit 1 ;;
+  esac
+done
+
+
+if [[ $target_flag == "web" ]]
+then
+  echo $target_flag
+  [ ! -d "build/" ] && mkdir build
+  cd build
+  emcmake cmake .. -DPLATFORM=Web -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXE_LINKER_FLAGS="-s USE_GLFW=3" -DCMAKE_EXECUTABLE_SUFFIX=".html"
+  emmake make
+  cd ..
+else
+  cmake -B build
+  cmake --build build
+fi
